c58bdd1b378c418ad26fbd9b0631b32e0b5af9b2
notvitalik <abhbasu7@gmail.com>
2026-01-04 07:33:10 -0500

    Simplify standalone branch to vanilla html/css/js implementation

--- lib/rng.ts
+++
@@ -1,9 +0,0 @@
-import seedrandom from 'seedrandom';
-
-export function rng(seed: number) {
-  return seedrandom(String(seed));
-}
-
-export function randomSeed() {
-  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-}
